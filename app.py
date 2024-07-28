2d79cce8daf148350aab0eb5b8c08bc9d3aab041
Nambukeerthi <nambu935@gmail.com>
2024-07-28 17:39:16 +0530

    calculation added

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import math
+import pandas as pd
+import numpy as np
 st.title('SPHERE CALCULATION')
 number = st.number_input("Insert a number")
-if st.button("Calculate"):
-  st.write("the number is", number)
+def sphere_volume(radius):
+    volume = (4/3) * math.pi * (radius ** 3)
+    return volume
+def sphere_surface_area(radius):
+    surface_area = 4 * math.pi * (radius ** 2)
+    return surface_area
+def main():
+    st.title('SPHERE CALCULATION')
+    number = st.number_input("Insert a number")
+    if st.button("Calculate"):
+        vol = sphere_volume(number)
+        area = sphere_surface_area(number)
+        st.write("The volume is ", vol)
+        st.write("The area  is ", area)  
+
